7bedb75a6ae0e65739104456d23973ba2589f696
Kevin Cristensen <ke.cristensen@duocuc.cl>
2024-08-05 18:58:36 -0400

    Anadiendo mensajes al provider

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -5,13 +5,17 @@ import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier{
   
-  List<Message> message = [
+  List<Message> messageList = [
     Message(text: 'Hola amor!', fromWho: FromWho.me),
     Message(text: 'Ya regresaste del trabajo amor?', fromWho: FromWho.me),
   ];
 
   Future<void> sendMessage( String text ) async {
-    //TODO: Implementar el metodo de envio de mensajes
+    final newMessage = Message(text: text, fromWho: FromWho.me);
+
+    messageList.add(newMessage);
+
+    notifyListeners();
   }
   
 }
